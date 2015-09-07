0be338efd38ee143ff9d4b1e8a7e552f77f314e2
Simone Dalmasso <simone.dalmasso@gmail.com>
2015-09-07 09:45:54 +0200

    fix tests

--- activation_viewer/activation/populate_test_data.py
+++ activation_viewer/activation/populate_test_data.py
@@ -2,61 +2,60 @@ from geonode.layers.models import Layer
 
 from .models import Activation, MapProduct, DisasterType
 
-activation_data = [
-    {
-        'activation_id': 'activ1',
-        'bbox_x0': 20,
-        'bbox_x1': 30,
-        'bbox_y0': 20,
-        'bbox_y1': 30,
-        'date': "2015-07-17T04:23:12",
-        'glide_number': "glide1",
-        'disaster_type': 1,
-        'service_level': 1, 
-    }, 
-    {
-        'activation_id': 'activ2',
-        'bbox_x0': 40,
-        'bbox_x1': 40,
-        'bbox_y0': 40,
-        'bbox_y1': 40,
-        'date': "2015-07-17T04:12:21",
-        'glide_number': 'glide2',
-        'disaster_type': 1,
-        'service_level': 1, 
-    }
-]
 
-map_product_data = [
-    {
-        'name': 'mp1',
-        'activation': 1,
-        'layers': [1, 2],
-        'type': 'reference',
-        'bbox_x0': 25,
-        'bbox_x1': 27,
-        'bbox_y0': 25,
-        'bbox_y1': 27,
-    },
-    {
-        'name': 'mp2',
-        'activation': 2,
-        'layers': [3, 4],
-        'type': 'grading',
-        'bbox_x0': 24,
-        'bbox_x1': 29,
-        'bbox_y0': 24,
-        'bbox_y1': 29,
-    }
-]
 
 def create_activation_data():
+
+    activation_data = [
+        {
+            'activation_id': 'activ1',
+            'bbox_x0': 20,
+            'bbox_x1': 30,
+            'bbox_y0': 20,
+            'bbox_y1': 30,
+            'date': "2015-07-17T04:23:12",
+            'glide_number': "glide1",
+            'service_level': 1, 
+        }, 
+        {
+            'activation_id': 'activ2',
+            'bbox_x0': 40,
+            'bbox_x1': 40,
+            'bbox_y0': 40,
+            'bbox_y1': 40,
+            'date': "2015-07-17T04:12:21",
+            'glide_number': 'glide2',
+            'service_level': 1, 
+        }
+    ]
+
+    map_product_data = [
+        {
+            'name': 'mp1',
+            'activation': 1,
+            'layers': [1, 2],
+            'type': 'reference',
+            'bbox_x0': 25,
+            'bbox_x1': 27,
+            'bbox_y0': 25,
+            'bbox_y1': 27,
+        },
+        {
+            'name': 'mp2',
+            'activation': 2,
+            'layers': [3, 4],
+            'type': 'grading',
+            'bbox_x0': 24,
+            'bbox_x1': 29,
+            'bbox_y0': 24,
+            'bbox_y1': 29,
+        }
+    ]
 
     flood = DisasterType.objects.create(name='Flood', slug='flood')
     eq = DisasterType.objects.create(name='Earthquake', slug='eq')
 
     for activation in activation_data:
-        activation.pop('disaster_type')
         Activation.objects.create(disaster_type=flood, **activation)
 
     for mp_data in map_product_data:
--- activation_viewer/activation/tests.py
+++ activation_viewer/activation/tests.py
@@ -7,29 +7,47 @@ from geonode.people.models import Profile
 from .populate_test_data import create_activation_data
 from .models import Activation
 
+
+activation_list_url = reverse(
+                            'api_dispatch_list',
+                            kwargs={
+                                'api_name': 'api',
+                                'resource_name': 'activations'})
+
 class ActivationTest(ResourceTestCase):
     """Activation tests"""
 
-    fixtures = ['bobby']
-    
+    fixtures = ['initial_data.json', 'bobby']
+
     def setUp(self):
+
+        super(ActivationTest, self).setUp()
+
         create_models(type='layer')
         create_activation_data()
+        self.user = 'admin'
+        self.passwd = 'admin'
 
     def test_activation_permissions(self):
         bobby = Profile.objects.get(username='bobby')
-        act = Activation.objects.get(id=1)
+        activation = Activation.objects.get(id=1)
         self.assertFalse(bobby.has_perm('view_activation', activation))
 
+    def test_activations_list_get_api_non_auth(self):
+        response = self.api_client.get(activation_list_url)
+        self.assertValidJSONResponse(response)
+        self.assertEquals(len(self.deserialize(response)['objects']), 0)
 
-    def test_activations_list_get_api(self):
-        response = self.api_client.get(reverse('activation_browse'))
-        self.assertValidJSONResponse(resp)
-        self.assertEquals(len(self.deserialize(resp)['objects']), 2)
+    def test_activations_list_get_api_auth(self):
+        self.api_client.client.login(username=self.user, password=self.passwd)        
+        response = self.api_client.get(activation_list_url)
+        self.assertValidJSONResponse(response)
+        self.assertEquals(len(self.deserialize(response)['objects']), 2)
 
     def test_activations_detail_get_api(self):
-        response = self.api_client.get(reverse('activation_details', kwargs={'activation_id': 1}))
-        self.assertValidJSONResponse(resp)
+        self.api_client.client.login(username=self.user, password=self.passwd)
+        response = self.api_client.get(reverse('activation_detail', 
+                kwargs={'activation_id': Activation.objects.all()[0].activation_id}))
         self.assertEquals(response.status_code, 200)
 
     def test_activation_permission_propagates_to_its_map_products(self):
