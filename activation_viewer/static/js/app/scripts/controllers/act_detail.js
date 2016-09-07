aad9db8149d866372b1c5e70fcde5f019c85ae82
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-09-07 12:53:18 +0200

    move buttons to the snippet

--- activation_viewer/static/js/app/scripts/controllers/act_detail.js
+++ activation_viewer/static/js/app/scripts/controllers/act_detail.js
@@ -23,6 +23,20 @@
 
       var map = olData.getMap();
 
+      $scope.zoomToActivation = function(act_id){
+        var activation = ActServices.activations.get(act_id).activation;
+        var extent = [activation.bbox_x0, activation.bbox_y0, activation.bbox_x1, activation.bbox_y1];
+        $rootScope.$emit('ZoomToExtent', extent);
+      };
+
+      $scope.toggleActivationLayers = function(action, act_id){
+        if(action == true){
+          $scope.$broadcast('addActivationLayers', act_id);
+        }else{
+          $scope.$broadcast('hideActivationLayers');
+        }
+      };
+
       $scope.toggleExternalLayer = function(url, layer_name){
         $scope.$broadcast('toggleExternalLayer', url, layer_name);
       }
