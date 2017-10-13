fb8e463fc3d68fbfbb45f2792c45821c1a05bb13
Simone Dalmasso <simone.dalmasso@gmail.com>
2017-10-13 14:40:11 +0200

    add load_activation command

---
+++ activation_viewer/loader/management/commands/loadactivation.py
@@ -0,0 +1,39 @@
+from optparse import make_option
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+
+from activation_viewer.loader.tasks import getActivation
+
+
+class Command(BaseCommand):
+
+    help = 'Load a CEMS activation'
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '-c',
+            '--code',
+            action='store_true',
+            dest='activation_code',
+            help='Actiovation\'s code'),
+        make_option(
+            '-r',
+            '--region',
+            action='store_true',
+            dest='region',
+            help='Activation\'s region'),
+        make_option(
+            '-t',
+            '--type',
+            action='store_true',
+            dest='disaster_type',
+            help='The disaster type'))
+
+    def handle(self, **options):
+        code = options.get('activation_code')
+        region = options.get('region')
+        disaster_type = options.get('disaster_type')
+
+        print 'Loading activation %s, %s in %s' % (code, disaster_type, region)
+        getActivation.delay(code, disaster_type, region)
