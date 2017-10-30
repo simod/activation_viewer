2de37642d9f53bae408c8f46d64fbb1911664bf1
maririv <i.mari.rivero@gmail.com>
2017-10-30 16:34:35 +0100

    Update styles.py

--- activation_viewer/loader/styles.py
+++ activation_viewer/loader/styles.py
@@ -61,7 +61,7 @@ styles = {
   </NamedLayer>
 </StyledLayerDescriptor>""",
 
-'a':
+'fire_grading_a':
 """<?xml version="1.0" encoding="ISO-8859-1"?>
 <StyledLayerDescriptor version="1.0.0"
   xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"
@@ -69,27 +69,160 @@ styles = {
   xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 
   <NamedLayer>
-    <Name>act_viewer_poly</Name>
+    <Name>Fire_grading</Name>
     <UserStyle>
-      <Title>A orange polygon style</Title>
+      <Title>Only filled polygons</Title>
       <FeatureTypeStyle>
+""" Grading for fires consists in three grades symbolized by filled polygons with no outline red, orange and yellow"""      
         <Rule>
-          <Title>orange polygon</Title>
+          <Title>Destroyed</Title>
+          <Filter>
+            <PropertyIsEqualTo>
+              <PropertyName>damage_gra</PropertyName>
+              <Literal>Destroyed</Literal>
+            </PropertyIsEqualTo>
+          </Filter>
           <PolygonSymbolizer>
             <Fill>
-              <CssParameter name="fill">#ff6600
+              <CssParameter name="fill">#F5293E
               </CssParameter>
             </Fill>
+          </PolygonSymbolizer>
+        </Rule>
+       
+        <Rule>
+          <Title>Damaged</Title>
+          <Filter>
+            <PropertyIsEqualTo>
+              <PropertyName>damage_gra</PropertyName>
+              <Literal>Damaged</Literal>
+            </PropertyIsEqualTo>
+          </Filter>
+          <PolygonSymbolizer>
+            <Fill>
+              <CssParameter name="fill">#F7A902
+              </CssParameter>
+            </Fill>
+          </PolygonSymbolizer>
+        </Rule>
+        
+        <Rule>
+          <Title>Probably damaged</Title>
+          <Filter>
+            <PropertyIsEqualTo>
+              <PropertyName>damage_gra</PropertyName>
+              <Literal>Probably damaged</Literal>
+            </PropertyIsEqualTo>
+          </Filter>
+          <PolygonSymbolizer>
+            <Fill>
+              <CssParameter name="fill">#ffffb7
+              </CssParameter>
+            </Fill>
+          </PolygonSymbolizer>
+        </Rule>
+
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>""",
+
+'building_grading_a':
+"""<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0"
+  xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"
+  xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc"
+  xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+  <NamedLayer>
+    <Name>Building_grading</Name>
+    <UserStyle>
+      <Title>Only outlined polygons</Title>
+      <FeatureTypeStyle>
+""" Building grading consists in three grades symbolized by not filled polygons red, orange and yellow"""      
+        <Rule>
+          <Title>Destroyed</Title>
+          <Filter>
+            <PropertyIsEqualTo>
+              <PropertyName>damage_gra</PropertyName>
+              <Literal>Destroyed</Literal>
+            </PropertyIsEqualTo>
+          </Filter>
+          <PolygonSymbolizer>
             <Stroke>
-              <CssParameter name="stroke">#000000</CssParameter>
+              <CssParameter name="stroke">#F5293E</CssParameter>
+              <CssParameter name="stroke-width">0.5</CssParameter>
+            </Stroke>  
+          </PolygonSymbolizer>
+        </Rule>
+       
+        <Rule>
+          <Title>Damaged</Title>
+          <Filter>
+            <PropertyIsEqualTo>
+              <PropertyName>damage_gra</PropertyName>
+              <Literal>Damaged</Literal>
+            </PropertyIsEqualTo>
+          </Filter>
+          <PolygonSymbolizer>
+            <Stroke>
+              <CssParameter name="stroke">#F7A902</CssParameter>
+              <CssParameter name="stroke-width">0.5</CssParameter>
+            </Stroke>  
+          </PolygonSymbolizer>
+        </Rule>
+        
+        <Rule>
+          <Title>Probably damaged</Title>
+          <Filter>
+            <PropertyIsEqualTo>
+              <PropertyName>damage_gra</PropertyName>
+              <Literal>Probably damaged</Literal>
+            </PropertyIsEqualTo>
+          </Filter>
+          <PolygonSymbolizer>
+             <Stroke>
+              <CssParameter name="stroke">#ffffb7</CssParameter>
               <CssParameter name="stroke-width">0.5</CssParameter>
             </Stroke>
           </PolygonSymbolizer>
+        </Rule>
 
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>""",
+
+'floods_a':
+"""<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0"
+  xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"
+  xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc"
+  xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+  <NamedLayer>
+    <Name>Flooded areas</Name>
+    <UserStyle>
+      <Title>Blue polygons</Title>
+      <FeatureTypeStyle>
+""" Flooded area is defined by a blue polygon, outline and fill the same blue  """      
+        <Rule>
+          <Title>Flooded area</Title>
+          <PolygonSymbolizer>
+            <Fill>
+              <CssParameter name="fill">#2DE2F0
+              </CssParameter>
+            </Fill>
+            <Stroke>
+              <CssParameter name="stroke">#2DE2F0</CssParameter>
+              <CssParameter name="stroke-width">0.5</CssParameter>
+            </Stroke>  
+          </PolygonSymbolizer>
         </Rule>
 
       </FeatureTypeStyle>
     </UserStyle>
   </NamedLayer>
 </StyledLayerDescriptor>"""
+    
 }
